2e2a8ae3ee03c3591637c88827d2319c3bcbc015
Dylan M. Taylor <dylan@dylanmtaylor.com>
2010-08-02 03:32:33 -0400

    added GPLv3 notices to source code

--- src/com/dylantaylor/ctb/CTB.java
+++ src/com/dylantaylor/ctb/CTB.java
@@ -1,4 +1,4 @@
-/* Touch The Ball by Dylan Taylor
+/* Catch The Ball by Dylan Taylor
  * ------------------------------
  * An attempt at making a basic game for the Android platform.
  * The idea is to have a basic circle bouncing around the screen, and have a
@@ -10,6 +10,21 @@
  *  Medium      9 Seconds
  *  Hard        6 Seconds
  *  Insane      3 Seconds
+ *
+ *  This file is part of Catch The Ball.
+ *
+ *  Catch The Ball is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Catch The Ball is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Catch The Ball.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.dylantaylor.ctb;
 
